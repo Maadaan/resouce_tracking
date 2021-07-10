b8cdad8fe00b76b850e1f06c6eb64a27aa757ac3
maadaan <chapagainmadan7@gmail.com>
2021-07-10 16:32:26 +0545

    user completed

--- apps/urls.py
+++ apps/urls.py
@@ -1,10 +1,26 @@
 from django.urls import path
-from .user_views import dashboard
+from .user_views import dashboard, hospital_list, hospital_detail, doctor_list, doctor_detail, volunteer_list, \
+    volunteer_detail, blood_list, blood_detail, hospital_related_list, hospital_related_detail
 
 urlpatterns = [
     # for admins urls
 
     # for users urls
-    path('', dashboard, name='dashboard')
+    path('', dashboard, name='dashboard'),
+
+    path('hospital_list/', hospital_list, name='hospital_list'),
+    path('hospital_detail/<int:pk>/', hospital_detail, name='hospital_detail'),
+
+    path('doctor_list/', doctor_list, name='doctor_list'),
+    path('doctor_detail/<int:pk>/', doctor_detail, name='doctor_detail'),
+
+    path('volunteer_list/', volunteer_list, name='volunteer_list'),
+    path('volunteer_detail/<int:pk>/', volunteer_detail, name='volunteer_detail'),
+
+    path('blood_list/', blood_list, name='blood_list'),
+    path('blood_detail/<int:pk>/', blood_detail, name='blood_detail'),
+
+    path('hospital_related_list/', hospital_related_list, name='hospital_related_list'),
+    path('hospital_related_detail/<int:pk>/', hospital_related_detail, name='hospital_related_detail'),
 
 ]
--- apps/user_views.py
+++ apps/user_views.py
@@ -23,15 +23,17 @@ def dashboard(request):
 
 # ------------------------------------------------------------
 def hospital_list(request):
+    hospitals = Hospital.objects.all()
     context = {
-
+        'hospitals': hospitals,
     }
     return render(request, 'user/hospital_list.html', context)
 
 
-def hospital_detail(request):
+def hospital_detail(request, pk):
+    hospitals = Hospital.objects.get(id=pk)
     context = {
-
+        'hospitals': hospitals,
     }
     return render(request, 'user/hospital_detail.html', context)
 
@@ -41,15 +43,17 @@ def hospital_detail(request):
 
 # ------------------------------------------------------------
 def doctor_list(request):
+    doctors = Doctor.objects.all()
     context = {
-
+        'doctors': doctors,
     }
     return render(request, 'user/doctor_list.html', context)
 
 
-def doctor_detail(request):
+def doctor_detail(request, pk):
+    doctors = Doctor.objects.get(id=pk)
     context = {
-
+        'doctors': doctors,
     }
     return render(request, 'user/doctor_detail.html', context)
 
@@ -59,15 +63,18 @@ def doctor_detail(request):
 
 # ------------------------------------------------------------
 def volunteer_list(request):
+    volunteers = Volunteer.objects.all()
     context = {
-
+        'volunteers': volunteers,
     }
     return render(request, 'user/volunteer_list.html', context)
 
 
-def volunteer_detail(request):
-    context = {
+def volunteer_detail(request, pk):
+    volunteers = Volunteer.objects.get(id=pk)
 
+    context = {
+        'volunteers': volunteers,
     }
     return render(request, 'user/volunteer_detail.html', context)
 
@@ -77,15 +84,17 @@ def volunteer_detail(request):
 
 # ------------------------------------------------------------
 def blood_list(request):
+    bloods = Blood.objects.all()
     context = {
-
+        'bloods': bloods,
     }
     return render(request, 'user/blood_list.html', context)
 
 
-def blood_detail(request):
+def blood_detail(request, pk):
+    bloods = Blood.objects.get(id=pk)
     context = {
-
+        'bloods': bloods,
     }
     return render(request, 'user/blood_detail.html', context)
 
@@ -95,15 +104,17 @@ def blood_detail(request):
 
 # ------------------------------------------------------------
 def hospital_related_list(request):
+    hospital_relates = HospitalRelated.objects.all()
     context = {
-
+        'hospital_relates': hospital_relates,
     }
     return render(request, 'user/hospital_related_list.html', context)
 
 
-def hospital_related_detail(request):
+def hospital_related_detail(request, pk):
+    hospital_relates = HospitalRelated.objects.get(id=pk)
     context = {
-
+        'hospital_relates': hospital_relates,
     }
     return render(request, 'user/hospital_related_detail.html', context)
 # ----------------------------------------------------------------
